f20ae3aceeedf8caa0bdc3f9e40883e0984151fd
makkiraza <makkirazaa@gmail.com>
2023-09-28 03:53:08 +0500

    msg

---
+++ app.js
@@ -0,0 +1,11 @@
+// for(var i = 1;i <= 10; i++){
+//     document.write(i+')loop <br>')
+// }
+
+// for (var i = 0; i <= 999; i++ ){
+//     document.write(i+)
+// }
+
+// document.write(2 + ' x ' + 1 + ' = ' + 2*1)
+
+for(var i=1)
